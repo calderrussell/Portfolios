0e74bcc7854730808840ca0e09962caaee30f9f5
Lex <lex.k.moulton@gmail.com>
2025-07-09 21:17:30 -0400

    Add router

--- portfolio-1/theme-init.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var savedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-
-    document.documentElement.setAttribute('data-theme', savedTheme);
-
-    if (!localStorage.getItem('theme')) {
-        localStorage.setItem('theme', savedTheme);
-    }
-})();
